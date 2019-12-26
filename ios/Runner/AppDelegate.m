7dae58a1cd771cefd9baa5a7fe5b606bf279fed4
pedia <ken.shao@gmail.com>
2019-12-26 15:53:03 +0800

    change display name add http transport permission

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,12 +1,15 @@
 #import "AppDelegate.h"
 #import "GeneratedPluginRegistrant.h"
 
+#import "WecastPlugin.h"
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
+  [WecastPlugin registerWithRegistrar:[self registrarForPlugin:@"wecast"]];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
